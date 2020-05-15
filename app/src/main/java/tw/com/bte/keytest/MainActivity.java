d65fa7ffeddbbcd8547a8097594f3b5273982f89
Jinder <jinder.wang@gmail.com>
2020-05-15 20:37:31 +0800

    Add plunger test

--- app/src/main/java/tw/com/bte/keytest/MainActivity.java
+++ app/src/main/java/tw/com/bte/keytest/MainActivity.java
@@ -1,13 +1,19 @@
 package tw.com.bte.keytest;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.InputDevice;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.InputStream;
@@ -32,13 +38,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button b_voldown;
     private Button b_enter;
     private Button b_esc;
+    private Button b_value;
+
+    private final static int SCANCODE_ENTER=28;
+    private final static int SCANCODE_ESCAPE=1;
+    private final static int SCANCODE_P=25;
+    private final static int SCANCODE_C=46;
+    private final static int SCANCODE_DPAD_LEFT=105;
+    private final static int SCANCODE_DPAD_RIGHT=106;
+    private final static int SCANCODE_DPAD_UP=103;
+    private final static int SCANCODE_DPAD_DOWN=108;
+    private final static int SCANCODE_VOLUME_DOWN=114;
+    private final static int SCANCODE_VOLUME_UP=115;
+    private final static int SCANCODE_B=305;
+    private final static int SCANCODE_X=307;
+    private final static int SCANCODE_THUMBR=318;
 
     private Button s1, s2, s3, s4, s5, s6;
+    private TextView tvADValue;
 
+    private ProgressBar plungerbar;
+    private int nKeyRepeatCount = 0;
+
+    private AlertDialog dialogQuit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        plungerbar = (ProgressBar) findViewById(R.id.plungerbar);
+        plungerbar.setProgress(100, true);
 
         //Uart_Init();
         //Button btn = (Button) findViewById(R.id.button);
@@ -132,49 +160,55 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         s5.setOnClickListener(this);
         s6 = findViewById(R.id.s6);
         s6.setOnClickListener(this);
+        tvADValue = findViewById(R.id.tvADValue);
     }
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        switch (keyCode){
-            case KeyEvent.KEYCODE_SHIFT_LEFT:
+        Log.d("KEY", "Keycode:" + keyCode + " Event:" + event + " Repeat Count:" + event.getRepeatCount()) ;
+        switch (event.getScanCode()){
+            case SCANCODE_X:
                 b_lshift.setBackgroundResource(R.color.colorButtonClick);
                 break;
-            case KeyEvent.KEYCODE_C:
+            case SCANCODE_C:
                 b_c.setBackgroundResource(R.color.colorButtonClick);
                 break;
-            case KeyEvent.KEYCODE_O:
+            case SCANCODE_THUMBR:
                 b_o.setBackgroundResource(R.color.colorButtonClick);
                 break;
-            case KeyEvent.KEYCODE_P:
+            case SCANCODE_P:
                 b_p.setBackgroundResource(R.color.colorButtonClick);
                 break;
-            case KeyEvent.KEYCODE_SHIFT_RIGHT:
+            case SCANCODE_B:
                 b_rshift.setBackgroundResource(R.color.colorButtonClick);
                 break;
-            case KeyEvent.KEYCODE_DPAD_LEFT:
+            case SCANCODE_DPAD_LEFT:
                 b_left.setBackgroundResource(R.color.colorButtonClick);
                 break;
-            case KeyEvent.KEYCODE_DPAD_UP:
+            case SCANCODE_DPAD_UP:
                 b_up.setBackgroundResource(R.color.colorButtonClick);
                 break;
-            case KeyEvent.KEYCODE_DPAD_DOWN:
+            case SCANCODE_DPAD_DOWN:
                 b_down.setBackgroundResource(R.color.colorButtonClick);
                 break;
-            case KeyEvent.KEYCODE_DPAD_RIGHT:
+            case SCANCODE_DPAD_RIGHT:
                 b_right.setBackgroundResource(R.color.colorButtonClick);
                 break;
-            case KeyEvent.KEYCODE_VOLUME_UP:
+            case SCANCODE_VOLUME_UP:
                 b_volup.setBackgroundResource(R.color.colorButtonClick);
                 break;
-            case KeyEvent.KEYCODE_VOLUME_DOWN:
+            case SCANCODE_VOLUME_DOWN:
                 b_voldown.setBackgroundResource(R.color.colorButtonClick);
                 break;
-            case KeyEvent.KEYCODE_ENTER:
+            case SCANCODE_ENTER:
                 b_enter.setBackgroundResource(R.color.colorButtonClick);
                 return true;
-            case KeyEvent.KEYCODE_BACK:
+            case SCANCODE_ESCAPE:
                 b_esc.setBackgroundResource(R.color.colorButtonClick);
+                nKeyRepeatCount = event.getRepeatCount();
+                if(nKeyRepeatCount > 28){
+                    ShowAlertFinish();
+                }
                 return true;
         }
 
@@ -182,51 +216,102 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
+    public boolean onGenericMotionEvent(MotionEvent event) {
+        if ((event.getSource() & InputDevice.SOURCE_JOYSTICK) ==
+                InputDevice.SOURCE_JOYSTICK &&
+                event.getAction() == MotionEvent.ACTION_MOVE) {
+            float value = event.getAxisValue(MotionEvent.AXIS_RY);
+            Log.d("KEY", "RY: " + value);
+            tvADValue.setText(String.valueOf(value));
+
+            if(value > 0.1){
+                plungerbar.setProgress(100-(int)(value*100));
+            } else {
+                plungerbar.setProgress(100);
+            }
+        }
+
+        return super.onGenericMotionEvent(event);
+    }
+
+    @Override
     public boolean onKeyUp(int keyCode, KeyEvent event) {
-        switch (keyCode){
-            case KeyEvent.KEYCODE_SHIFT_LEFT:
+        Log.d("KEY", "Keycode:" + keyCode + " Event:" + event);
+        switch (event.getScanCode()){
+            case SCANCODE_X:
                 b_lshift.setBackgroundResource(R.color.colorButton);
                 break;
-            case KeyEvent.KEYCODE_C:
+            case SCANCODE_C:
                 b_c.setBackgroundResource(R.color.colorButton);
                 break;
-            case KeyEvent.KEYCODE_O:
+            case SCANCODE_THUMBR:
                 b_o.setBackgroundResource(R.color.colorButton);
                 break;
-            case KeyEvent.KEYCODE_P:
+            case SCANCODE_P:
                 b_p.setBackgroundResource(R.color.colorButton);
                 break;
-            case KeyEvent.KEYCODE_SHIFT_RIGHT:
+            case SCANCODE_B:
                 b_rshift.setBackgroundResource(R.color.colorButton);
                 break;
-            case KeyEvent.KEYCODE_DPAD_LEFT:
+            case SCANCODE_DPAD_LEFT:
                 b_left.setBackgroundResource(R.color.colorButton);
                 break;
-            case KeyEvent.KEYCODE_DPAD_UP:
+            case SCANCODE_DPAD_UP:
                 b_up.setBackgroundResource(R.color.colorButton);
                 break;
-            case KeyEvent.KEYCODE_DPAD_DOWN:
+            case SCANCODE_DPAD_DOWN:
                 b_down.setBackgroundResource(R.color.colorButton);
                 break;
-            case KeyEvent.KEYCODE_DPAD_RIGHT:
+            case SCANCODE_DPAD_RIGHT:
                 b_right.setBackgroundResource(R.color.colorButton);
                 break;
-            case KeyEvent.KEYCODE_VOLUME_UP:
+            case SCANCODE_VOLUME_UP:
                 b_volup.setBackgroundResource(R.color.colorButton);
                 break;
-            case KeyEvent.KEYCODE_VOLUME_DOWN:
+            case SCANCODE_VOLUME_DOWN:
                 b_voldown.setBackgroundResource(R.color.colorButton);
                 break;
-            case KeyEvent.KEYCODE_ENTER:
+            case SCANCODE_ENTER:
                 b_enter.setBackgroundResource(R.color.colorButton);
                 break;
-            case KeyEvent.KEYCODE_BACK:
+            case SCANCODE_ESCAPE:
                 b_esc.setBackgroundResource(R.color.colorButton);
                 return true;
         }
+
         return super.onKeyUp(keyCode, event);
     }
 
+    private void ShowAlertFinish(){
+        AlertDialog.Builder  builder = new AlertDialog.Builder(this);
+        final View dialogview = this.getLayoutInflater().inflate(R.layout.dialog_finish_app, null);
+        builder.setView(dialogview);
+        Button btnYes = (Button) dialogview.findViewById(R.id.btnYes);
+        btnYes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogQuit.cancel();
+                finish();
+            }
+        });
+
+        Button btnNo = (Button) dialogview.findViewById(R.id.btnNo);
+        btnNo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialogQuit.cancel();
+            }
+        });
+
+        dialogQuit = builder.create();
+        dialogQuit.show();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        //super.onBackPressed();
+    }
 
     private void Uart_Init(){
         try {
